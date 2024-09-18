566a01d2b28ce138fa3f9fe8f69a0f9cd03aa2d0
Sheikh Shihabun Sakib <46291833+sakibskib@users.noreply.github.com>
2024-09-17 20:56:17 -0500

    Classwork End point right before checkpoint 5

--- class1/src/App.jsx
+++ class1/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
+import Calendar from './components/Calendar'
 
 const App = () => {
 
   return (
     <div className="App">
 
-      
+      <h1>TimeTabled</h1>
+      <h2>Welcome to Chicago, Natalia! check out this calendar to get to know the city and see all the sights during your stay</h2>
+      <Calendar />
     </div>
   )
 }
---
+++ class1/src/components/Calendar.jsx
@@ -0,0 +1,125 @@
+import React from "react";
+import Event from './Event'
+
+const Calendar = () => {
+  return (
+    <div className="Calendar">
+      <table>
+        <tr>
+          <th></th>
+          <th>Sunday</th>
+          <th>Monday</th>
+          <th>Tuesday</th>
+          <th>Wednesday</th>
+          <th>Thursday</th>
+          <th>Friday</th>
+          <th>Saturday</th>
+        </tr>
+        <tbody>
+          <tr>
+            <td className="time">8am</td>
+            <Event event='Fancy Dinner' color ='green'/>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td className="time">9am</td>
+            <td> </td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td className="time">10am</td>
+            <td> </td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td className="time">11am</td>
+            <td> </td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td className="time">12pm</td>
+            <td> </td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td className="time">1pm</td>
+            <td> </td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td className="time">2pm</td>
+            <td> </td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td className="time">3pm</td>
+            <td> </td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td className="time">4pm</td>
+            <td> </td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td className="time">5pm</td>
+            <td> </td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Calendar;
