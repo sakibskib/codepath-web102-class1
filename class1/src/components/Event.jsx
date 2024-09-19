c45b01a794e52b67af13dd442b5f5a16ef5efbbb
Sheikh Shihabun Sakib <46291833+sakibskib@users.noreply.github.com>
2024-09-18 21:08:34 -0500

    chore: Update Calendar component with events and colors

--- class1/src/components/Event.jsx
+++ class1/src/components/Event.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 const Event = (props) => {
     return (
-        <td className="Event">
+        <td className={'Event ' + props.color}>
             <h5>{props.event}</h5>
+            <h6>{props.location}</h6>
         </td>
     )
 }
